c1f16a6c5cf7b385b1fac1de32a6b843a65d0fde
Silver369dragon <yf100031@gmail.com>
2022-04-06 16:53:31 +0800

    上傳

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: [
+    "./index.html",
+    "./title-page.html",
+    "./about-club.html",
+    "./about-Technical.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
